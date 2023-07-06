3ed044dbcf745121b86933e0f04ad6c5a374ed3a
tranthekhiem14112001 <trankhiem99999@gmail.com>
2023-07-06 08:18:24 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Welcome from "./components/Welcom.jsx";
+
+function App() {
+    return (
+        <div className='App'>
+            <Welcome name='Admin'/>
+        </div>
+    )
+}
+
+export default App
